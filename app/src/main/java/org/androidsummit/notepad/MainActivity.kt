58719e783870621746362b9c672b9833ea99c9bb
Aaron Sarazan <aaron@levelmoney.com>
2017-08-18 11:13:24 -0700

    remove view from lambda param

--- app/src/main/java/org/androidsummit/notepad/MainActivity.kt
+++ app/src/main/java/org/androidsummit/notepad/MainActivity.kt
@@ -13,8 +13,8 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+        fab.setOnClickListener {
+            Snackbar.make(it, "Replace with your own action", Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show()
         }
     }
